a51322796824b68e532669c8a6f9cb3c47cf2480
qpm <kangqiang.w@gmail.com>
2020-12-08 23:50:42 +0800

    learn: 17章 套接字编程的 examples2

--- src/c17/socket_examples.erl
+++ src/c17/socket_examples.erl
@@ -10,11 +10,11 @@
 -author("qpm").
 
 %% API
--export([nano_get_url/1]).
+-export([nano_get_url/1, start_nano_server/0, nano_client_eval/1]).
 
 nano_get_url(Host) ->
   {ok, Socket} = gen_tcp:connect(Host, 80, [binary, {packet, 0}]),
-  ok = gen_tcp:send(Socket, "GET / HTTP/1.0|r|n|r|n"),
+  ok = gen_tcp:send(Socket, "GET / HTTP/1.0\r\n\r\n"),
   receive_data(Socket, []).
 
 
@@ -24,4 +24,37 @@ receive_data(Socket, SoFar) ->
       receive_data(Socket, [Bin|SoFar]);
     {tcp_closed, Socket} ->
       list_to_binary(lists:reverse(SoFar))
+  end.
+
+start_nano_server() ->
+  {ok, Listen} = gen_tcp:listen(2345, [binary,{packet, 4}, {reuseaddr, true}, {active, true}]),
+  {ok, Socket} = gen_tcp:accept(Listen),
+  gen_tcp:close(Listen),
+  loop(Socket).
+
+
+loop(Socket) ->
+  receive
+    {tcp, Socket, Bin} ->
+      io:format("Server received binary = ~p~n", [Bin]),
+      Str = binary_to_term(Bin),
+      io:format("server (unpacked) ~p~n", [Str]),
+      Replay = ["I Got " | Str],
+      io:format("Server replying = ~p~n", [Replay]),
+      gen_tcp:send(Socket, term_to_binary((Replay))),
+      loop(Socket);
+    {tcp_closed, Socket} ->
+      io:format("server socket closed ~n")
+  end.
+
+
+nano_client_eval(Str) ->
+  {ok, Socket} = gen_tcp:connect("localhost", 2345, [binary, {packet, 4}]),
+  ok = gen_tcp:send(Socket, term_to_binary(Str)),
+  receive
+    {tcp, Socket, Bin} ->
+      io:format("Client received binary = ~p~n", [Bin]),
+      Val = binary_to_term(Bin),
+      io:format("Client result = ~p~n",[Val]),
+      gen_tcp:close(Socket)
   end.
